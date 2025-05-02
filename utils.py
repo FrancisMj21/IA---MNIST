c0442ebe66ab1e9c4ed7e42812fb60fe31050130
FrancisMj21 <francisc0der2023@gmail.com>
2025-05-01 23:41:15 -0500

    Update and rename utils.ipynb to utils.py

---
+++ utils.py
@@ -0,0 +1,13 @@
+class MyConvBlock(nn.Module):
+    def __init__(self, in_ch, out_ch, dropout_p):
+        super().__init__()
+        self.model = nn.Sequential(
+            nn.Conv2d(in_ch, out_ch, kernel_size=3, padding=1),
+            nn.BatchNorm2d(out_ch),
+            nn.ReLU(),
+            nn.Dropout(dropout_p),
+            nn.MaxPool2d(2)
+        )
+
+    def forward(self, x):
+        return self.model(x)
